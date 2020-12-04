ed9efab50bdc39deef83bcfa8dd973d1c199caaf
Dera Ezenwa <74721651+Dera9q@users.noreply.github.com>
2020-12-04 17:46:45 +0100

    can add and run migration

--- src/Todoweb.data/Entities/ApplicationUser.cs
+++ src/Todoweb.data/Entities/ApplicationUser.cs
@@ -4,6 +4,6 @@ namespace Todoweb.data.Entities
 {
     public class ApplicationUser : IdentityUser
     {
-        
+        public string FullName { get; set; }
     }
 }
---
+++ src/Todoweb.data/Migrations/ApplicationDb/20201204160800_Initial Application Migration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Todoweb.data.Migrations.ApplicationDb
+{
+    public partial class InitialApplicationMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todolist",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todolist", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todolist");
+        }
+    }
+}
