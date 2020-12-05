534f4493bb25cf6a7d38c9588ab70c9665da3f79
Dera Ezenwa <74721651+Dera9q@users.noreply.github.com>
2020-12-05 20:37:42 +0100

    add models for register and login

--- src/TodoWeb.web/Controllers/AccountController.cs
+++ src/TodoWeb.web/Controllers/AccountController.cs
@@ -1,17 +1,35 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
+using TodoWeb.web.Models;
 
 namespace TodoWeb.web.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if(!ModelState.IsValid) return View();
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if(!ModelState.IsValid) return View();
+            throw new NotImplementedException();
         }
     }
 }
